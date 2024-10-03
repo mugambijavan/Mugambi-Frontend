b0a199fd46cc3ce8780df83963193be44db0a2cf
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-10-04 00:56:27 +0300

    "Removed Work component and 'work' navigation item, added NavigationDots component"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import { About, Footer, Testimonial, Work } from './container';
-import { Navbar } from './components';
+import { About, Footer, Testimonial } from './container';
 import './App.scss';
 
 const App = () => (
   <div className="app">
-    <Navbar />
     <About />
-    <Work />
     <Testimonial />
     <Footer />
   </div>
